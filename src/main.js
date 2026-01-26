885dc4ba9a69eb79d76d7257491f99a40848dda9
lushuo <lushuo@kedacom.com>
2026-01-26 11:42:27 +0800

    fix: Element Plus MessageBox 和 Notification 样式引入

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './styles/tailwind.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./styles/tailwind.css";
+// 导入 Element Plus 样式（用于 MessageBox、Notification 等组件）
+import "element-plus/es/components/message/style/css";
+import "element-plus/es/components/message-box/style/css";
+import "element-plus/es/components/notification/style/css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
